f24dc2f7def250d7244c95edec56448dc8583ee8
dnlchristanto <dnl.christanto@gmail.com>
2024-10-22 16:14:53 +0700

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\HomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login',[LoginController::class,'index'])->name('login')->middleware('guest');
+Route::post('/login',[LoginController::class,'authenticate'])->name('login.authenticate');
+// Route::get('/logout',[LoginController::class,'logout'])->middleware('auth');
+Route::post('/logout', function () {
+    Auth::logout();
+    request()->session()->invalidate();
+    request()->session()->regenerateToken();
+    return redirect('/');
+})->name('logout')->middleware('auth');
+Route::get('/register',[RegisterController::class,'index'])->name('register')->middleware('guest');
+Route::post('/register',[RegisterController::class,'store'])->name('register.store');
+Route::get('/home',[HomeController::class,'index'])->middleware('auth');
